d4ae81a2d23e6d5c1700e5032d45e779f0259620
Kartik Dhiman <59189590+kartikdhiman@users.noreply.github.com>
2026-06-29 14:57:46 +0530

    feat: add deployments for Redis, RabbitMQ, and Solar UI to cluster setup

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -29,6 +29,11 @@ kubectl wait \
   --selector=db=solar-postgres \
   --timeout=180s
 
+printf "\n📦 Deploying Redis......"
+kubectl apply -f services/redis.yml
+
+sleep 5
+
 printf "\n⌛ Waiting for Redis to be deployed..."
 
 while [ "$(kubectl get pod -l db=solar-redis | wc -l)" -eq 0 ] ; do
@@ -42,6 +47,11 @@ kubectl wait \
   --selector=db=solar-redis \
   --timeout=180s
 
+printf "\n📦 Deploying RabbitMQ......"
+kubectl apply -f services/rabbitmq.yml
+
+sleep 5
+
 printf "\n⌛ Waiting for RabbitMQ to be deployed..."
 
 while [ "$(kubectl get pod -l db=solar-rabbitmq | wc -l)" -eq 0 ] ; do
@@ -53,6 +63,23 @@ printf "\n⌛ Waiting for RabbitMQ to be ready..."
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=solar-rabbitmq \
+  --timeout=180s
+
+# minikube image load solar-ui:latest --profile solar
+printf "\n📦 Deploying Solar UI..."
+kubectl apply -f services/solar-ui.yml
+
+sleep 5
+
+printf "\n⌛ Waiting for Solar UI to be deployed..."
+while [ "$(kubectl get pod -l app=solar-ui | wc -l)" -eq 0 ] ; do
+  sleep 5
+done
+
+printf "\n⌛ Waiting for Solar UI to be ready..."
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=solar-ui \
   --timeout=180s
 
 printf "\n⛵ Happy Sailing!\n"
