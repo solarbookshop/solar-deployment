9806298295b7802a859d02112fb13de77c23a1b9
Kartik Dhiman <59189590+kartikdhiman@users.noreply.github.com>
2026-06-29 14:58:50 +0530

    feat: add Keycloak deployment and configuration to cluster setup

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -1,29 +1,41 @@
 #!/bin/sh
 
 printf "\n📦 Initializing Kubernetes cluster...\n"
-
 minikube start --cpus 2 --memory 4g --driver docker --profile solar
 
 printf "\n🔌 Enabling NGINX Ingress Controller...\n"
-
 minikube addons enable ingress --profile solar
 
 sleep 15
 
-printf "\n📦 Deploying platform services..."
+printf "\n📦 Deploying Keycloak..."
+kubectl apply -f services/keycloak-config.yml
+kubectl apply -f services/keycloak.yml
 
-kubectl apply -f services
+sleep 5
+
+printf "\n⌛ Waiting for Keycloak to be deployed..."
+while [ "$(kubectl get pod -l app=solar-keycloak | wc -l)" -eq 0 ] ; do
+  sleep 5
+done
+
+printf "\n⌛ Waiting for Keycloak to be ready..."
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=solar-keycloak \
+  --timeout=300s
+
+printf "\n📦 Deploying PostgreSQL......"
+kubectl apply -f services/postgresql.yml
 
 sleep 5
 
 printf "\n⌛ Waiting for PostgreSQL to be deployed..."
-
 while [ "$(kubectl get pod -l db=solar-postgres | wc -l)" -eq 0 ] ; do
   sleep 5
 done
 
 printf "\n⌛ Waiting for PostgreSQL to be ready..."
-
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=solar-postgres \
@@ -35,13 +47,11 @@ kubectl apply -f services/redis.yml
 sleep 5
 
 printf "\n⌛ Waiting for Redis to be deployed..."
-
 while [ "$(kubectl get pod -l db=solar-redis | wc -l)" -eq 0 ] ; do
   sleep 5
 done
 
 printf "\n⌛ Waiting for Redis to be ready..."
-
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=solar-redis \
@@ -53,13 +63,11 @@ kubectl apply -f services/rabbitmq.yml
 sleep 5
 
 printf "\n⌛ Waiting for RabbitMQ to be deployed..."
-
 while [ "$(kubectl get pod -l db=solar-rabbitmq | wc -l)" -eq 0 ] ; do
   sleep 5
 done
 
 printf "\n⌛ Waiting for RabbitMQ to be ready..."
-
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=solar-rabbitmq \
